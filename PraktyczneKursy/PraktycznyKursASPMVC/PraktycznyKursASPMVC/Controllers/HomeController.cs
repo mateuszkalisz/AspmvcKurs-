5353990837b94101ddfc36c8f6c04f8239b9563b
MK <kaliszmat1@gmail.com>
2019-01-07 18:47:23 +0100

    asp mvc training, create application

---
+++ PraktyczneKursy/PraktycznyKursASPMVC/PraktycznyKursASPMVC/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PraktycznyKursASPMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            // komentarz
+            return View();
+        }
+    }
+}
